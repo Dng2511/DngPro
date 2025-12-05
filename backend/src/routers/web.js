d605fa7ba323036be0c8662467213a284e8da6cc
Dng Pham <phamdong9854@gmail.com>
2025-12-05 23:14:12 +0700

    update security

--- backend/src/routers/web.js
+++ backend/src/routers/web.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+// Controllers
+const UserController = require('../apps/controllers/user');
+
+
+
+// Middlewares
+const checkLoggedIn = require('../apps/middlewares/checkLoggedIn');
+
+
 router.get('/', (req, res) => {
     res.send("API is working");
 });
+
+router.post('/register/customer', UserController.registerForCustomer);
+router.post('/login/:role', UserController.login);
+router.post('/register/staff', checkLoggedIn('admin'), UserController.registerForStaff);
 
 
 
